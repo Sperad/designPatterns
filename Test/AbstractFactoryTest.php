ade344a77d3235cfdff465cc6485ed764c96da6d
jpher <sperad.com@gmail.com>
2016-12-18 16:57:13 +0800

    AbstractFactory testJsonText

--- Test/AbstractFactoryTest.php
+++ Test/AbstractFactoryTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Test;
 use design\AbstractFactory\HtmlFactory;
+use design\AbstractFactory\JsonFactory;
 
 class AbstractFactoryTest extends \PHPUnit_Framework_TestCase
 {
@@ -10,6 +11,15 @@ class AbstractFactoryTest extends \PHPUnit_Framework_TestCase
         $content = 'htmlContent';
         $text = $factory->createText($content);
         $this->assertInstanceOf('design\AbstractFactory\HtmlText', $text);
+        $this->assertEquals($content, $text->getText());
+    }
+
+    public function testJsonText()
+    {
+        $factory = new JsonFactory();
+        $content = 'jsonContent';
+        $text = $factory->createText($content);
+        $this->assertInstanceOf('design\AbstractFactory\JsonText', $text);
         $this->assertEquals($content, $text->getText());
     }
 }
